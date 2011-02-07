267bb966298edaf330ec26a43447cf70deb50106
Andrew Schaaf <andrew@andrewschaaf.com>
2011-02-06 20:32:29 -0500

    `node-bitcoin-impl --new-key`

--- build-coffee/main.js
+++ build-coffee/main.js
@@ -1,7 +1,22 @@
 (function() {
-  var n;
-  n = require('./../build-cc/default/native');
-  exports.new_keypair = n.new_keypair;
-  exports.pubkey_to_address256 = n.pubkey_to_address256;
-  exports.base58_encode = n.base58_encode;
+  var base58_encode, new_keypair, pubkey_to_address256, _ref;
+  _ref = require('./../build-cc/default/native'), new_keypair = _ref.new_keypair, pubkey_to_address256 = _ref.pubkey_to_address256, base58_encode = _ref.base58_encode;
+  exports.new_keypair = new_keypair;
+  exports.pubkey_to_address256 = pubkey_to_address256;
+  exports.base58_encode = base58_encode;
+  exports.main = function() {
+    var opt, priv, pub, _ref;
+    opt = require('optimist').argv;
+    if (opt['new-key']) {
+      _ref = new_keypair(), priv = _ref[0], pub = _ref[1];
+      return process.stdout.write((JSON.stringify({
+        private64: priv.toString('base64'),
+        public64: pub.toString('base64'),
+        bitcoin_address: base58_encode(pubkey_to_address256(pub))
+      })) + '\n');
+    } else {
+      console.log("Invalid args");
+      return process.exit(1);
+    }
+  };
 }).call(this);
--- coffee/main.coffee
+++ coffee/main.coffee
@@ -1,8 +1,22 @@
 
+{new_keypair, pubkey_to_address256, base58_encode} = require './../build-cc/default/native'
 
-n = require './../build-cc/default/native'
 
-exports.new_keypair = n.new_keypair
-exports.pubkey_to_address256 = n.pubkey_to_address256
-exports.base58_encode = n.base58_encode
+exports.new_keypair = new_keypair
+exports.pubkey_to_address256 = pubkey_to_address256
+exports.base58_encode = base58_encode
+
+
+exports.main = () ->
+  opt = require('optimist').argv
+  if opt['new-key']
+    [priv, pub] = new_keypair()
+    process.stdout.write((JSON.stringify {
+      private64: priv.toString 'base64'
+      public64: pub.toString 'base64'
+      bitcoin_address: base58_encode(pubkey_to_address256(pub))
+    }) + '\n')
+  else
+    console.log "Invalid args"
+    process.exit 1
 
